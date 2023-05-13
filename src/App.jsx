c4c34ce19b957082289cf3a5d35253b332db83fb
Arvind <thakurarvindkr10@gmail.com>
2023-05-13 10:13:36 +0530

    stop the function to re-creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './App.css';
 import DemoOutput from './components/Demo/DemoOuput';
@@ -6,17 +6,26 @@ import Button from './components/UI/Button';
 
 function App() {
     const [showParagraph, setShowParagraph] = useState(false);
+    const [allowToggle, setAllowToggle] = useState(false);
 
     console.log('APP RUNNING');
 
-    const toggleParagraphHandler = () => {
-        setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    };
+    const toggleParagraphHandler = useCallback(() => {
+        if (allowToggle) {
+            console.log('allowToggle', allowToggle)
+            setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+        }
+    }, [allowToggle]);
+
+    const allowToggleHandler = () => {
+        setAllowToggle(!allowToggle)
+    }
 
     return (
         <div className="app">
             <h1>Hi there :)</h1>
             <DemoOutput show={showParagraph} />
+            <Button onClick={allowToggleHandler}>Allow Toggle</Button><br /><br />
             <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
         </div>
     );
