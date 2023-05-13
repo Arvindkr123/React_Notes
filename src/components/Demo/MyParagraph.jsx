020d9f69a3a4a04ee52320db51c3fefd3bfb173c
Arvind <thakurarvindkr10@gmail.com>
2023-05-13 08:01:32 +0530

    component re-Evaluation

---
+++ src/components/Demo/MyParagraph.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyParagraph = ({ children }) => {
+    console.log('MyParagraph running ..........')
+    return (
+        <p>
+            {children}
+        </p>
+    )
+}
+
+export default MyParagraph
