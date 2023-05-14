8133b4b403b2c762fbed07d6e3924a875a6c3193
Arvind <thakurarvindkr10@gmail.com>
2023-05-14 12:41:41 +0530

    added started code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import LocalStorage from './components/LocalStorage.jsx'
-
-const App = () => {
+import "react-toastify/dist/ReactToastify.css";
+import LoginPage from "./Pages/LoginPage.jsx";
+import UsersList from "./Pages/UsersList.jsx";
+import HomePage from "./Pages/HomePage.jsx";
+import { ToastContainer } from "react-toastify";
+import Footer from "./Components/Footer.jsx";
+import { Route, Routes } from "react-router-dom";
+function App() {
     return (
-        <div>
-            🌬️ Learning Local Storage Hello 🤪
-            <LocalStorage />
-        </div>
-    )
+        <>
+            <ToastContainer
+                position="top-center"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/profile" element={<HomePage />} />
+                <Route path="/users" element={<UsersList />} />
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
