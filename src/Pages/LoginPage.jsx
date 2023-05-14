5b919f202668db7244ff6b4e1e5a55689baf8870
Arvind <thakurarvindkr10@gmail.com>
2023-05-14 16:23:19 +0530

    UserManagementSystem

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -3,6 +3,20 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const userName = localStorage.getItem('email') ? localStorage.getItem('email') : 'admin@admin.com'
+    const userPassword = localStorage.getItem('password') ? localStorage.getItem('password') : 'admin';
+    const submitHandler = e => {
+        e.preventDefault();
+        if (userName === email && password === userPassword) {
+            toast.success('Login Success');
+            navigate('/profile');
+        } else {
+            toast.error('Invalid Email or Password')
+        }
+    }
     return (
         <>
             <div className="form__container d-flex felx-column align-items-center justify-content-center">
@@ -14,6 +28,8 @@ const LoginPage = () => {
                             Email address
                         </label>
                         <input
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
                             type="email"
                             className="form-control"
                             id="exampleInputEmail1"
@@ -25,6 +41,8 @@ const LoginPage = () => {
                             Password
                         </label>
                         <input
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
                             type="password"
                             className="form-control"
                             id="exampleInputPassword1"
@@ -35,7 +53,7 @@ const LoginPage = () => {
                             Don't Have An Account? <Link to="/">Signup !</Link>
                         </p>
                     </div>
-                    <button type="submit" className="form__button">
+                    <button type="submit" className="form__button" onClick={submitHandler}>
                         Login
                     </button>
                 </form>
